9b4422a14170a5e1d23df666ae8ab1377337c019
Dmitry <dmitrijkotov634@gmail.com>
2022-08-30 20:53:14 +0300

    fix: Install apk

--- app/src/main/java/com/wavecat/kotovstore/InstallWorker.kt
+++ app/src/main/java/com/wavecat/kotovstore/InstallWorker.kt
@@ -52,6 +52,7 @@ class InstallWorker(appContext: Context, workerParams: WorkerParameters) :
 
         val intent = Intent(Intent.ACTION_VIEW)
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
         intent.setDataAndType(
             FileProvider.getUriForFile(
